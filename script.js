c0a8ac3fee63b40843a14d8ca0430aa7210d836d
sincdxb <yaolimingdxb@gmail.com>
2026-06-17 13:12:51 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const images = [
-  "Coffee/B0DW7BXKRH/01-main.jpg",
-  "Coffee/B0DW7BXKRH/02.jpg",
-  "Coffee/B0DW7BXKRH/03.jpg"
-];
 
-let index = 0;
+const images = {
+  coffee1: [
+    "Coffee/B09LHVL6W1/01.jpg",
+    "Coffee/B09LHVL6W1/02.jpg",
+    "Coffee/B09LHVL6W1/03.jpg"
+  ],
+  kitchen1: [
+    "Kitchen/B08CKTB6Z4/01.jpg",
+    "Kitchen/B08CKTB6Z4/02.jpg",
+    "Kitchen/B08CKTB6Z4/03.jpg"
+  ],
+  home1: [
+    "Home/B0G9Z319YP/01.jpg",
+    "Home/B0G9Z319YP/02.jpg",
+    "Home/B0G9Z319YP/03.jpg"
+  ]
+};
 
-function showImg() {
-  const img = document.getElementById("productImage");
-  if (!img) return;
-  img.src = images[index];
+const indexMap = {
+  coffee1:0,
+  kitchen1:0,
+  home1:0
+};
+
+function show(id){
+  document.getElementById("img-"+id).src = images[id][indexMap[id]];
 }
 
-function nextImg() {
-  index = (index + 1) % images.length;
-  showImg();
+function next(id){
+  indexMap[id] = (indexMap[id]+1)%images[id].length;
+  show(id);
 }
 
-function prevImg() {
-  index = (index - 1 + images.length) % images.length;
-  showImg();
+function prev(id){
+  indexMap[id] = (indexMap[id]-1+images[id].length)%images[id].length;
+  show(id);
 }
